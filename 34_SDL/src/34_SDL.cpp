27a702815caaf974f68ddb5f1090159e1ba20dd3
Mert Acel <mert.acel@ardictech.com>
2017-09-27 22:43:45 +0300

    SDL Window

--- 34_SDL/src/34_SDL.cpp
+++ 34_SDL/src/34_SDL.cpp
@@ -18,33 +18,69 @@ int main() {
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "SDL Init Failed..." << endl;
-		 SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
 		return 1;
 	}
 
-	SDL_Window *window=SDL_CreateWindow("Test Window",
-			SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED,
-			SCREEN_WIDTH,
-			SCREEN_HEIGHT,
-			SDL_WINDOW_SHOWN);
+	SDL_Window *window = SDL_CreateWindow("Test Window",
+	SDL_WINDOWPOS_UNDEFINED,
+	SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-	if(window==NULL){
+	if (window == NULL) {
 		SDL_Quit();
 		return 2;
 	}
 
-	bool quit =false;
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+			SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	if (renderer == NULL) {
+		cout << "SDL renderer Failed..." << endl;
+		SDL_Log("Unable to renderer SDL: %s", SDL_GetError());
+
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+
+	if (texture == NULL) {
+		cout << "SDL texture Failed..." << endl;
+		SDL_Log("Unable to texture SDL: %s", SDL_GetError());
+
+		SDL_DestroyTexture(texture);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	Uint32 *buffer=new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
+	memset(buffer,255,SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+
+	SDL_UpdateTexture(texture,
+			NULL,
+			buffer,
+			SCREEN_WIDTH*sizeof(Uint32));
+
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer,texture,NULL,NULL);
+	SDL_RenderPresent(renderer);
+
+	bool quit = false;
 	SDL_Event event;
 
-	while(!quit){
-		while(SDL_PollEvent(&event)){
-			if(event.type== SDL_QUIT){
-				quit=true;
+	while (!quit) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
 			}
 		}
 	}
 
+	delete(buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(texture);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
