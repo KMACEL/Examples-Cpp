102b71bc325724d4163498ecca9801b0235fbb04
Mert Acel <mert.acel@ardictech.com>
2017-10-09 17:02:32 +0300

    SDL Animation

--- 35_SDL_Classes/src/35_SDL_Classes.cpp
+++ 35_SDL_Classes/src/35_SDL_Classes.cpp
@@ -7,7 +7,7 @@
 //============================================================================
 
 #include <iostream>
-
+#include <math.h>
 #include "Screen.h"
 using namespace std;
 
@@ -22,9 +22,20 @@ int main() {
 			break;
 		}
 
-		screen.setPixel(300, 250, 255, 255, 255);
-		screen.setPixel(300, 251, 255, 255, 255);
-		screen.setPixel(300, 252, 255, 255, 255);
+		int elapsed = SDL_GetTicks();
+		unsigned char green =
+				(unsigned char) ((1 + sin(elapsed * 0.0001)) * 128);
+		unsigned char red = (unsigned char) ((1 + sin(elapsed * 0.0002)) * 128);
+		unsigned char blue = (unsigned char) ((1 + sin(elapsed * 0.0003)) * 128);
+
+		for (int y = 0; y < screen.SCREEN_HEIGHT; y++) {
+			for (int x = 0; x < screen.SCREEN_WIDTH; x++) {
+				screen.setPixel(x, y, red, green, blue);
+			}
+		}
+		/*screen.setPixel(300, 250, 255, 255, 255);
+		 screen.setPixel(300, 251, 255, 255, 255);
+		 screen.setPixel(300, 252, 255, 255, 255);*/
 		screen.update();
 	}
 
